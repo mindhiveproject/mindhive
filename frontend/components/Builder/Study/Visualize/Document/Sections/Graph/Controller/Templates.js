18eeb9cdb95cd98d48bb860357de003169db7826
FranckPrts <franck.porteous@protonmail.com>
2024-04-05 19:48:33 -0400

    Added py template for graph + retun null for empty row on compute func

--- frontend/components/Builder/Study/Visualize/Document/Sections/Graph/Controller/Templates.js
+++ frontend/components/Builder/Study/Visualize/Document/Sections/Graph/Controller/Templates.js
@@ -1,19 +1,245 @@
 export default function TemplateSelector({ handleChange, runCode, sectionId }) {
-  const sectionCodeStart = `import js_workspace as data
+
+  const sectionCodeStart = ``;
+
+  // const linePlotCode = `fig = px.line(df, x=X, y=Y, color=Group)`;
+  const scatterPlotCode = `
+
+# indicate the name of the column where the participant id is defined
+id_col = 'participant' # MH uses 'participant' but you uploaded data may use a different name!
+
+Group=''
+
+# TEXT-BOXES
+legend_title='LEGEND'
+
+# CHECK-BOXES
+add_trend_line = False
+
+######################################################################################################
+######################### Don't you dare change anything below #######################################
+######################################################################################################
+
+# Currently takes the name of the parameter given in GUI input
+axis_title = {
+  X: X if xLabel == "" else xLabel, 
+  Y: Y if yLabel == "" else yLabel
+}
+
+optional_params = {
+  "marginal_x": "rug" if marginalPlot == 'rug' else "box" if marginalPlot=='box' else None,
+  "marginal_y": "rug" if marginalPlot == 'rug' else "box" if marginalPlot=='box' else None,
+}
+
+import numpy as np
+import pandas as pd
+import matplotlib.colors as mcolors
+import js_workspace as data
 data = data.to_py()
 df = pd.DataFrame(data)
 
 # convert string to numbers
-df[X] = pd.to_numeric(df[X])
-df[Y] = pd.to_numeric(df[Y])`;
+columns = [X, Y]
+df[columns] = df[columns].apply(pd.to_numeric, errors='coerce')
 
-  const sectionCodeEnd = `fig_html = fig.to_html()
+# Convert 'NaN' strings to actual NaN values
+df.replace('NaN', np.nan, inplace=True)
+
+# Define custom aggregation function to handle NaN values
+def agg_func(x):
+    non_nan_values = x.dropna()
+    if non_nan_values.empty:
+        return np.nan
+    else:
+        return non_nan_values.iloc[0]
+# agg_func = lambda x: x.dropna().iloc[0]
+
+# Group by participant ID and aggregate data
+if Group == '':
+  grouped_df = df.groupby(id_col).agg({
+      X: agg_func,  # Take the first non-NaN x value for each participant
+      Y: agg_func   # Take the first non-NaN y value for each participant
+  }).reset_index()
+else:
+  grouped_df = df.groupby(id_col).agg({
+      X: agg_func,  # Take the first non-NaN x value for each participant
+      Y: agg_func,  # Take the first non-NaN y value for each participant
+      Group: agg_func,  # Take the first non-NaN y value for each participant
+  }).reset_index()
+
+if Group=='':
+  if add_trend_line:
+    
+    print("dl statsmodels")
+    await micropip.install("statsmodels")
+    print("finish dl statsmodels")
+    print('NOgroup-addtrendline')
+    fig = px.scatter(grouped_df, x=X, y=Y, 
+                      trendline="ols", 
+                      title = graphTitle if graphTitle != '' else f"{X} vs {Y}",
+                      labels=axis_title, 
+                      template="seaborn",
+                      **{k: v for k, v in optional_params.items() if v})
+    fig.update_layout(showlegend=False)
+
+
+  else:
+    print('NOgroup-NOTaddtrendline')
+    fig = px.scatter(grouped_df, x=X, y=Y,
+                      title=graphTitle if graphTitle != '' else f"{X} vs {Y}",
+                      labels=axis_title, 
+                      template="seaborn",
+                      **{k: v for k, v in optional_params.items() if v})
+    fig.update_layout(showlegend=False)
+else:
+  if add_trend_line:
+    print('group-addtrendline')
+
+    print("dl statsmodels")
+    await micropip.install("statsmodels")
+    print("finish dl statsmodels")
+    
+    fig = px.scatter(grouped_df, x=X, y=Y, 
+                      trendline="ols", 
+                      title=graphTitle if graphTitle != '' else f"{X} vs {Y}",
+                      color=Group,
+                      labels=axis_title, 
+                      template="seaborn",
+                      **{k: v for k, v in optional_params.items() if v})
+    fig.update_layout(showlegend=False)
+    # for trace in fig.data:
+    #   trace.showlegend = False
+  else:
+    print('group-NOTaddtrendline')
+    fig = px.scatter(grouped_df, x=X, y=Y, 
+                      color=Group, 
+                      title=graphTitle if graphTitle != '' else f"{X} vs {Y}",
+                      labels=axis_title, 
+                      template="seaborn",
+                      **{k: v for k, v in optional_params.items() if v})
+    # fig.update_layout(showlegend=False)  
+  `;
+  const histogramCode = `
+#~ OPTIONS ~#
+# float input
+bargap = 0.1
+#nbins = ""
+
+######################################################################################################
+######################### Don't you dare change anything below #######################################
+######################################################################################################
+
+optional_params = {
+  "marginal": "rug" if marginalPlot == 'rug' else "box" if marginalPlot=='box' else None,
+}
+
+import numpy as np
+import pandas as pd
+import matplotlib.colors as mcolors
+import js_workspace as data
+
+data = data.to_py()
+df = pd.DataFrame(data)
+
+df[X] = df[X].apply(pd.to_numeric, errors='coerce')
+
+fig = px.histogram(df, x=X, 
+                    #nbins=nbins, 
+                    **{k: v for k, v in optional_params.items() if v})
+
+fig.update_layout(title=graphTitle if graphTitle != '' else f"Histogram of {X}",
+                  bargap=bargap,
+                  xaxis_title_text=xLabel if xLabel != '' else X,
+                  yaxis_title_text=yLabel if yLabel != '' else "Count"
+                  )
+
+`;
+  const barGraphCode = `
+#Example 1 (2 cols)
+#columns = ['GT_gamble_percentage_mixed', 'GT_gamble_percentage_lose']
+#labels = ['Mixed-condition', 'Lose-condition']
+
+#Example 2 (3 cols)
+columns = ['GT_gamble_percentage_mixed', 'GT_gamble_percentage_lose', 'GT_gamble_percentage_gain']
+labels = ['Mixed', 'Lose', 'Gain']
+
+legend_title='Condition'
+
+#Choose a base color for your graph
+base_color = 'pink'
+
+######################################################################################################
+######################### Don't you dare change anything below #######################################
+######################################################################################################
+
+title= graphTitle if graphTitle != "" else f"Barplot of {columns}"
+
+import js_workspace as data
+data = data.to_py()
+df = pd.DataFrame(data)
+  
+import numpy as np
+import pandas as pd
+import matplotlib.colors as mcolors
+
+df[columns] = df[columns].apply(pd.to_numeric, errors='coerce')
+
+# Calculate mean, standard deviation, and confidence intervals for each column
+statistics = {}
+
+for column in columns:
+    mean = np.mean(df[column])
+    std = np.std(df[column])
+    n = df[column].shape[0]
+    conf_interval = 1.96 * np.sqrt(std**2 / n)  # 95% confidence interval
+    statistics[column] = {'mean': mean, 'std': std, 'conf_interval': conf_interval}
+
+# Extract the required statistics for plotting
+average_percentages = [statistics[column]['mean'] for column in columns]
+error_bars = [statistics[column]['conf_interval'] for column in columns]
+
+def generate_complementary_colors(base_color, n):
+    base_rgb = mcolors.to_rgb(base_color)
+    complementary_rgb = mcolors.rgb_to_hsv(base_rgb)
+    complementary_rgb[0] = (complementary_rgb[0] + 0.5) % 1  # Shift hue by 180 degrees (complementary color)
+    
+    # Generate n harmonious colors
+    harmonious_colors = []
+    for _ in range(n):
+        complementary_rgb[0] = (complementary_rgb[0] + 0.2) % 1  # Shift hue by 72 degrees (for example)
+        harmonious_colors.append(mcolors.to_hex(mcolors.hsv_to_rgb(complementary_rgb)))
+    
+    return harmonious_colors
+  
+n = len(columns)  # Number of harmonious colors to generate
+colors = generate_complementary_colors(base_color, n)
+
+#['blue', 'orange', 'green']
+
+df_bar = pd.DataFrame({
+    'Categories': labels,
+    'Y': average_percentages,
+    'Error Bars': error_bars
+})
+
+fig = px.bar(df_bar, 
+              x='Categories', 
+              y='Y', 
+              color='Categories',
+              error_y='Error Bars', 
+              color_discrete_sequence=colors,
+              labels={'Categories': xLabel if xLabel != "" else "Categories",
+                      'Y': yLabel if yLabel != "" else 'Average value'
+                      })
+fig.update_layout(
+    title=title,
+    legend_title=legend_title
+)`;
+
+  const sectionCodeEnd = `  
+fig_html = fig.to_html()
 js.render_html(plot_output, fig_html)`;
 
-  const linePlotCode = `fig = px.line(df, x=X, y=Y, color=Group)`;
-  const scatterPlotCode = `fig = px.scatter(df, x=X, y=Y, color=Group)`;
-  const histogramCode = `fig = px.histogram(df, x=X)`;
-  const barGraphCode = `fig = px.bar(df, x=X, y=Y, color=Group)`;
 
   const templates = {
     // linePlot: sectionCodeStart + "\n" + linePlotCode + "\n" + sectionCodeEnd,
--- frontend/components/Builder/Study/Visualize/Process/Operations/Compute.js
+++ frontend/components/Builder/Study/Visualize/Process/Operations/Compute.js
@@ -3,22 +3,52 @@ import { Modal, Dropdown } from "semantic-ui-react";
 import { StyledForm } from "../../../../../styles/StyledForm";
 
 const operations = [
-  { value: "add", text: " + " },
-  { value: "subtract", text: " - " },
-  { value: "multiply", text: " x " },
-  { value: "divide", text: " / " },
+  { value: "zeroState", text: "" },  
+  { value: "add", text: " Addition (+) " },
+  { value: "subtract", text: " Substraction (-) " },
+  { value: "multiply", text: " Multiplication (x) " },
+  { value: "divide", text: " Division (/) " },
+  { value: "average", text: " Average (x̄) " },
 ];
 
-const compute = ({ var1, var2, operation }) => {
-  switch (operation) {
+const compute = ({ var1, var2, data, selectedVariables, operation }) => {
+  switch(operation) {
     case "add":
-      return var1 + var2;
+      return (isNaN(var1) || isNaN(var2)) ? null : var1 + var2;
     case "subtract":
-      return var1 - var2;
+      console.log("var1", var1)
+      console.log("var2", var2)
+      console.log((isNaN(var1) || isNaN(var2)))
+      return (isNaN(var1) || isNaN(var2)) ? null : var1 - var2;
     case "multiply":
-      return var1 * var2;
+      return (isNaN(var1) || isNaN(var2)) ? null : var1 * var2;
     case "divide":
-      return var1 / var2;
+      return (isNaN(var1) || isNaN(var2)) ? null : var1 / var2;
+    case "average":
+      if (Array.isArray(selectedVariables) && selectedVariables.length > 0) {
+        // Calculate the average of selected variables, ignoring undefined values
+        const validValues = selectedVariables
+          .map(variable => parseFloat(data[variable]))
+          .filter(value => !isNaN(value)); // Filter out NaN (undefined) values
+        console.log(validValues)
+        const sum = validValues.reduce((acc, value) => acc + value, 0);
+        return validValues.length > 0 ? sum / validValues.length : 0;
+      } else {
+        return 42; // Return 0 if selectedVariables is not an array or is empty
+      }
+      
+
+      // case "average":
+    //   if (Array.isArray(selectedVariables) && selectedVariables.length > 0) {
+    //     console.log(data)
+    //     return selectedVariables.reduce((sum, variable) => sum + parseFloat(data[variable]), 0) / selectedVariables.length;
+    //     // return selectedVariables.reduce((sum, variable) => sum + variable, 0) / selectedVariables.length;
+    //   } else {
+    //     return 2; // Return 0 if selectedVariables is not an array or is empty
+    //   }
+
+    default:
+      throw new Error(`Unsupported operation: ${operation}`);
   }
 };
 
@@ -53,6 +83,8 @@ export default function Compute({
       [inputs?.name]: compute({
         var1: parseFloat(row[inputs?.variable1]),
         var2: parseFloat(row[inputs?.variable2]),
+        data: data,
+        selectedVariables: Array.isArray(inputs?.selectedVariables) ? inputs.selectedVariables : [],
         operation: inputs?.operation,
       }),
     }));
@@ -80,21 +112,6 @@ export default function Compute({
                 value={inputs?.name}
                 onChange={handleChange}
               />
-
-              <label>First variable</label>
-              <Dropdown
-                placeholder="Select variable"
-                fluid
-                search
-                selection
-                options={variablesOptions}
-                onChange={(event, data) =>
-                  handleChange({
-                    target: { name: "variable1", value: data?.value },
-                  })
-                }
-              />
-
               <label>Operation</label>
               <Dropdown
                 placeholder="Select operation"
@@ -108,20 +125,155 @@ export default function Compute({
                   })
                 }
               />
+              {inputs?.operation === "add" && (
+                <div>
+                <label>First variable</label>
+                <Dropdown
+                  placeholder="Select variable"
+                  fluid
+                  search
+                  selection
+                  options={variablesOptions}
+                  onChange={(event, data) =>
+                    handleChange({
+                      target: { name: "variable1", value: data?.value },
+                    })
+                  }
+                />
 
-              <label>Second variable</label>
-              <Dropdown
-                placeholder="Select variable"
-                fluid
-                search
-                selection
-                options={variablesOptions}
-                onChange={(event, data) =>
-                  handleChange({
-                    target: { name: "variable2", value: data?.value },
-                  })
-                }
-              />
+<label>Second variable</label>
+                <Dropdown
+                  placeholder="Select variable"
+                  fluid
+                  search
+                  selection
+                  options={variablesOptions}
+                  onChange={(event, data) =>
+                    handleChange({
+                      target: { name: "variable2", value: data?.value },
+                    })
+                  }
+                />
+              </div>
+              )}
+              {inputs?.operation === "subtract" && (
+                <div>
+                <label>First variable</label>
+                <Dropdown
+                  placeholder="Select variable"
+                  fluid
+                  search
+                  selection
+                  options={variablesOptions}
+                  onChange={(event, data) =>
+                    handleChange({
+                      target: { name: "variable1", value: data?.value },
+                    })
+                  }
+                />
+
+                <label>Second variable</label>
+                <Dropdown
+                  placeholder="Select variable"
+                  fluid
+                  search
+                  selection
+                  options={variablesOptions}
+                  onChange={(event, data) =>
+                    handleChange({
+                      target: { name: "variable2", value: data?.value },
+                    })
+                  }
+                />
+              </div>
+
+              )}
+              {inputs?.operation === "multiply" && (
+                <div>
+                <label>First variable</label>
+                <Dropdown
+                  placeholder="Select variable"
+                  fluid
+                  search
+                  selection
+                  options={variablesOptions}
+                  onChange={(event, data) =>
+                    handleChange({
+                      target: { name: "variable1", value: data?.value },
+                    })
+                  }
+                />
+
+                <label>Second variable</label>
+                <Dropdown
+                  placeholder="Select variable"
+                  fluid
+                  search
+                  selection
+                  options={variablesOptions}
+                  onChange={(event, data) =>
+                    handleChange({
+                      target: { name: "variable2", value: data?.value },
+                    })
+                  }
+                />
+              </div>
+
+              )}
+              {inputs?.operation === "divide" && (
+                <div>
+                <label>First variable</label>
+                <Dropdown
+                  placeholder="Select variable"
+                  fluid
+                  search
+                  selection
+                  options={variablesOptions}
+                  onChange={(event, data) =>
+                    handleChange({
+                      target: { name: "variable1", value: data?.value },
+                    })
+                  }
+                />
+
+                <label>Second variable</label>
+                <Dropdown
+                  placeholder="Select variable"
+                  fluid
+                  search
+                  selection
+                  options={variablesOptions}
+                  onChange={(event, data) =>
+                    handleChange({
+                      target: { name: "variable2", value: data?.value },
+                    })
+                  }
+                />
+              </div>
+
+              )}
+              {inputs?.operation === "average" && (
+                <div>
+                  <label>Select variable(s):</label>
+                  <Dropdown
+                    placeholder="Select variable(s)"
+                    fluid
+                    multiple
+                    search
+                    selection
+                    options={variablesOptions}
+                    onChange={(event, data) => {
+                      const selectedVariables = data?.value;
+                      // console.log("Selected Variables:");
+                      // console.log("Selected Variables:", selectedVariables);
+                      handleChange({
+                        target: { name: "selectedVariables", value: selectedVariables },
+                      });
+                    }}
+                  />
+
+                </div>
+              )}
             </fieldset>
           </StyledForm>
         </Modal.Description>
