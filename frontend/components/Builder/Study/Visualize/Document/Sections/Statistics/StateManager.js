8c804f857e2bbcb3f2480fb9175c6f13342047a4
FranckPrts <franck.porteous@protonmail.com>
2024-04-05 23:35:08 -0400

    changed template for Summary Statistics

--- frontend/components/Builder/Study/Visualize/Document/Sections/Statistics/StateManager.js
+++ frontend/components/Builder/Study/Visualize/Document/Sections/Statistics/StateManager.js
@@ -13,10 +13,36 @@ import {
 import Render from "./Render";
 import CodeEditor from "./Controller/CodeEditor";
 
-const defaultCode = `import js_workspace as data
+const defaultCode = `
+
+column_input = ['GT_gamble_percentage_gain', 'GT_gamble_percentage_lose', 'GT_last_score']
+
+
+
+
+#############################################################################################
+######################### Don't change anything below #######################################
+#############################################################################################
+
+import js_workspace as data
+import numpy as np
 data = data.to_py()
 df = pd.DataFrame(data)
-df_html = df.to_html()
+
+df.replace('NaN', np.nan, inplace=True)
+described_df = pd.DataFrame()
+
+# Loop over the list of columns
+for column in column_input:
+    # Convert the column to numeric
+    df[column] = pd.to_numeric(df[column], errors='coerce') 
+    # Describe the column and add it to the described_df DataFrame
+    described_df = pd.concat([described_df, df[column].describe()], axis=1)
+
+# Put the ds in the correct orientation
+described_df = described_df.transpose()
+
+df_html = described_df.to_html()
 js.render_html(html_output, df_html)`;
 
 export default function StateManager({
