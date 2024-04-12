bf184757c225956344344ae667ac7c0667be7980
FranckPrts <franck.porteous@protonmail.com>
2024-04-11 21:09:58 -0400

    added tooltip on columns

--- frontend/components/Builder/Study/Visualize/Process/Main.js
+++ frontend/components/Builder/Study/Visualize/Process/Main.js
@@ -4,6 +4,21 @@ import { AgGridReact } from "ag-grid-react"; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 
+// @Yury, nned help making sure the func below has control 
+export function CustomTooltip(params) {
+  return (
+    <div style={{ 
+      padding: '10px', 
+      backgroundColor: '#272727', 
+      border: '10px solid #d4d4d4',
+      transition: 'opacity 0.3s', 
+      opacity: params.hovered ? '1' : '0'
+    }}>
+      {params.value}
+    </div>
+  );
+}
+
 export default function ProcessMain({ data, variables }) {
   // re-format columns definitions
   const colDefs = variables.map((variable) => ({
@@ -11,18 +26,27 @@ export default function ProcessMain({ data, variables }) {
     hide: variable?.hide,
     editable: variable?.editable,
     headerName: variable?.displayName || variable?.field,
+    headerTooltip: variable?.displayName || variable?.field,
+    tooltipComponent: 'customTooltip',
+    tooltipComponentParams: { hovered: true },
   }));
+  
 
   // apply settings across all columns
-  const defaultColDef = useMemo(() => ({
+  const defaultColDef = useMemo(() => {
+    return {
+      initialWidth: 400,
+      // wrapHeaderText: true,
+      // autoHeaderHeight: true,
+    };
     // filter: true,
     // floatingFilter: true,
-  }));
+  }, []);
 
   // settings
   const pagination = true;
-  const paginationPageSize = 500;
-  const paginationPageSizeSelector = [200, 500, 1000];
+  const paginationPageSize = 100;
+  const paginationPageSizeSelector = [50, 100, 200, 500, 1000];
 
   if (data && data?.length) {
     return (
@@ -38,8 +62,10 @@ export default function ProcessMain({ data, variables }) {
             pagination={pagination}
             paginationPageSize={paginationPageSize}
             paginationPageSizeSelector={paginationPageSizeSelector}
-            // onCellValueChanged={(event) => console.log({ event })}
+            frameworkComponents={{ customTooltip: CustomTooltip }}
           />
+            {/* // onCellValueChanged={(event) => console.log({ event })}
+          /> */}
         </div>
       </div>
     );
