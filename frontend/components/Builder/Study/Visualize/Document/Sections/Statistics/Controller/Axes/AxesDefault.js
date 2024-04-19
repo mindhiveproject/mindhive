77d9cde5a8f1135f0b9bc36f1e7e3bf65d1c7c70
FranckPrts <franck.porteous@protonmail.com>
2024-04-19 00:11:48 -0400

    adding the default Axes file for DescStat

---
+++ frontend/components/Builder/Study/Visualize/Document/Sections/Statistics/Controller/Axes/AxesDefault.js
@@ -0,0 +1,67 @@
+import SelectOne from "../Fields/SelectOne";
+
+export default function Axes({
+  type,
+  variables,
+  code,
+  pyodide,
+  runCode,
+  sectionId,
+  selectors,
+  handleContentChange,
+}) {
+  const connectSelectorsCode = `# get relevant html elements
+plot_output = js.document.getElementById('figure-${sectionId}')
+X = js.document.getElementById("xVariable-${sectionId}").value
+Y = js.document.getElementById("yVariable-${sectionId}").value
+Group = js.document.getElementById("groupVariable-${sectionId}").value`;
+
+  const options = variables.map((variable) => ({
+    key: variable?.field,
+    value: variable?.field,
+    text: variable?.displayName || variable?.field,
+  }));
+
+  const updateCode = async ({ code }) => {
+    await pyodide.runPythonAsync(connectSelectorsCode);
+    runCode({ code });
+  };
+
+  const onSelectorChange = ({ target }) => {
+    handleContentChange({
+      newContent: {
+        selectors: { ...selectors, [target?.name]: target?.value },
+      },
+    });
+    updateCode({ code });
+  };
+
+  return (
+    <div className="selectors">
+      <SelectOne
+        sectionId={sectionId}
+        options={options}
+        selectors={selectors}
+        onSelectorChange={onSelectorChange}
+        title="X-Axis"
+        parameter="xVariable"
+      />
+      <SelectOne
+        sectionId={sectionId}
+        options={options}
+        selectors={selectors}
+        onSelectorChange={onSelectorChange}
+        title="Y-Axis"
+        parameter="yVariable"
+      />
+      <SelectOne
+        sectionId={sectionId}
+        options={options}
+        selectors={selectors}
+        onSelectorChange={onSelectorChange}
+        title="Group"
+        parameter="groupVariable"
+      />
+    </div>
+  );
+}
