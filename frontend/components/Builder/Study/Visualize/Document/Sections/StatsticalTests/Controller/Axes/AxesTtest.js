f0a962a089abcfd5c0042de6a96df37c22d09f2c
FranckPrts <franck.porteous@protonmail.com>
2024-04-25 13:47:59 -0400

    added wide/long opts to T-TEST

--- frontend/components/Builder/Study/Visualize/Document/Sections/StatsticalTests/Controller/Axes/AxesTtest.js
+++ frontend/components/Builder/Study/Visualize/Document/Sections/StatsticalTests/Controller/Axes/AxesTtest.js
@@ -1,3 +1,7 @@
+import React, { useState } from "react";
+import { Dropdown, DropdownMenu } from "semantic-ui-react";
+
+import SelectMultiple from "../Fields/SelectMultiple";
 import SelectOne from "../Fields/SelectOne";
 
 export default function Axes({
@@ -10,49 +14,166 @@ export default function Axes({
   selectors,
   handleContentChange,
 }) {
-  const connectSelectorsCode = `# get relevant html elements
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const connectSelectorsCodeWide = `
+import json
 plot_output = js.document.getElementById('figure-${sectionId}')
-col1 = js.document.getElementById("col1-${sectionId}").value
-col2 = js.document.getElementById("col2-${sectionId}").value`;
 
-  const options = variables.map((variable) => ({
-    key: variable?.field,
-    value: variable?.field,
-    text: variable?.displayName || variable?.field,
-  }));
+col1 = None if js.document.getElementById("col1-${sectionId}") == None else js.document.getElementById("col1-${sectionId}").value
+col2 = None if js.document.getElementById("col2-${sectionId}") == None else js.document.getElementById("col2-${sectionId}").value
 
-  const updateCode = async ({ code }) => {
-    await pyodide.runPythonAsync(connectSelectorsCode);
-    runCode({ code });
-  };
+isWide = True
 
-  const onSelectorChange = ({ target }) => {
-    handleContentChange({
-      newContent: {
-        selectors: { ...selectors, [target?.name]: target?.value },
+#x_labels= columns ######################################need update################ To change to list under x axis
+`;
+const connectSelectorsCodeLong = `
+plot_output = js.document.getElementById('figure-${sectionId}')
+
+qualCol  = None if js.document.getElementById("qualCol-${sectionId}") == None else js.document.getElementById("qualCol-${sectionId}").value
+quantCol = None if js.document.getElementById("quantCol-${sectionId}") == None else js.document.getElementById("quantCol-${sectionId}").value
+
+isWide = False
+
+#x_labels= columns ######################################need update################ To change to list under x axis
+`;
+
+const options = variables.map((variable) => ({
+  key: variable?.field,
+  value: variable?.field,
+  text: variable?.displayName || variable?.field,
+}));
+
+const updateCode = async ({ code }) => {
+  if (selectedOption === "wide") {
+    await pyodide.runPythonAsync(connectSelectorsCodeWide);
+  } else if (selectedOption === "long") {
+    await pyodide.runPythonAsync(connectSelectorsCodeLong);
+  }
+  runCode({ code });
+};
+
+
+const onSelectorChoice = (option) => {
+  setSelectedOption(option.value);
+};
+
+const onSelectorChange = ({ target }) => {
+  const value = Array.isArray(target?.value) ? target?.value : [target?.value];
+  handleContentChange({
+    newContent: {
+      selectors: { ...selectors, [target?.name]: value },
+    },
+  });
+  updateCode({ code });
+};
+
+return (
+  <div className="selectorsStatTest">
+  <Dropdown 
+  className="dropdownMenu"
+    icon={
+      <div className="menuItemThreeDiv menuButton">
+        <img src={`/assets/icons/visualize/database_selected.svg`} />
+        <div>
+          {selectedOption ? (
+            <a><b>Click here</b> to change your choice.</a>
+          ) : (
+            <a><b>Click here</b> to select how the data for this analysis is structured.</a>
+          )}
+        </div>
+
+        <div></div>
+        {selectedOption && (
+          selectedOption === "long" ? (
+            <div>
+              You selected a <b>{selectedOption}</b>-data format dashboard
+            </div>
+            ) : (
+              <div>
+                You selected a <b>{selectedOption}</b>-data format dashboard
+              </div>
+              )
+          )
+        }
+      </div>
+    }
+  >
+  <DropdownMenu>
+    {[
+      { 
+        key: 'long', 
+        value: 'long', 
+        title: "Long Data Format", 
+        description: "Data organized with each observation (like a student's test score) appearing on its own row, often with a column indicating categories (like subjects). This format is useful for detailed analysis across categories.", 
+        img: '/assets/icons/visualize/dataStructLong.svg'
       },
-    });
-    updateCode({ code });
-  };
+      { 
+        key: 'wide', 
+        value: 'wide', 
+        title: "Wide Data Format", 
+        description: "Data organized with each category (like subjects) appearing as its own column, often with rows representing observations (like students). This format is simpler for quick comparisons within categories.", 
+        img: '/assets/icons/visualize/dataStructWide.svg'
+      }
+    ].map((option) => (
+      <div
+        key={option.key}
+        className="menuItemDataStruct menuButton"
+        onClick={() => onSelectorChoice(option)}
+      >
+        <img src={option.img} alt={option.title} />
+        <div>
+          <h3>{option.title}</h3>
+          <p>{option.description}</p>
+        </div>
+      </div>
+    ))}
+  </DropdownMenu>
 
-  return (
-    <div className="selectorsStats">
-      <SelectOne
-        sectionId={sectionId}
-        options={options}
-        selectors={selectors}
-        onSelectorChange={onSelectorChange}
-        title="1st Column"
-        parameter="col1"
-      />
-      <SelectOne
-        sectionId={sectionId}
-        options={options}
-        selectors={selectors}
-        onSelectorChange={onSelectorChange}
-        title="2nd Column"
-        parameter="col2"
-      />
-    </div>
-  );
+  </Dropdown>
+
+  {selectedOption && (
+    selectedOption === "long" ? (
+      <div className="selectorsStats">
+        <SelectOne
+          sectionId={sectionId}
+          options={options}
+          selectors={selectors}
+          onSelectorChange={onSelectorChange}
+          title="Qualitative Column"
+          parameter="qualCol"
+        />
+        <SelectOne
+          sectionId={sectionId}
+          options={options}
+          selectors={selectors}
+          onSelectorChange={onSelectorChange}
+          title="Quantitative Column"
+          parameter="quantCol"
+        />
+      </div>
+    ) : (
+      <div className="selectorsStats">
+        <SelectOne
+          sectionId={sectionId}
+          options={options}
+          selectors={selectors}
+          onSelectorChange={onSelectorChange}
+          title="1st Column"
+          parameter="col1"
+        />
+        <SelectOne
+          sectionId={sectionId}
+          options={options}
+          selectors={selectors}
+          onSelectorChange={onSelectorChange}
+          title="2nd Column"
+          parameter="col2"
+        />
+      </div>
+    )
+  )}
+</div>
+);
 }
