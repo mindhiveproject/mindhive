71f858f819b17173eb7997a716f4c104e0be3448
Yury-Shevchenko <shevchenko_yury@mail.ru>
2024-11-13 11:28:25 +0100

    fix uploaded data in dataviz

--- frontend/components/Builder/Study/Visualize/Overview/Browse/BrowseTemplates.js
+++ frontend/components/Builder/Study/Visualize/Overview/Browse/BrowseTemplates.js
@@ -67,7 +67,8 @@ export default function BrowseTemplates({ studyId, journal }) {
         input: {
           title: "Copy of " + template?.title,
           description: template?.description,
-          dataOrigin: "TEMPLATE",
+          dataOrigin:
+            template?.dataOrigin === "UPLOADED" ? "UPLOADED" : "TEMPLATE",
           settings: template?.settings,
           content: template?.content,
           vizChapters: {
