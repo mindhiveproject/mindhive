6e881f565dea3c3aa782088e2609d8440a4f4577
Franck <franck.porteous@protonmail.com>
2025-03-04 05:34:32 -0500

    Updating Feedback center, About page, add support ticket & block request to homepage (#23)
    * Added support ticket and block request buttons to homepage (but for students)
    
    * update text in About page

--- frontend/components/Dashboard/Home/Main.js
+++ frontend/components/Dashboard/Home/Main.js
@@ -40,14 +40,41 @@ export default function Home({ query, user }) {
           </div>
         </div>
 
-        <div>
+        <div className="idInfo">
           Permissions
           {user?.permissions.map((permission, num) => (
             <div key={num} className="code">
               {permission?.name}
             </div>
           ))}
+          <div>
+            {(user?.permissions?.map((p) => p?.name).includes("MENTOR") ||
+              user?.permissions?.map((p) => p?.name).includes("ADMIN") ||
+              user?.permissions?.map((p) => p?.name).includes("SCIENTIST") ||
+              user?.permissions?.map((p) => p?.name).includes("TEACHER")) && (
+              <div>
+                Do you need support?<br></br>
+                <a href="https://mindhive.notion.site/153d80abf4c48093b13cc8d50807c7b8?pvs=105" target="_blank">
+                  <button>Fill Support Ticket</button>
+                </a>
+              </div>
+            )}
+          </div>
+          <div>
+            {(user?.permissions?.map((p) => p?.name).includes("MENTOR") ||
+              user?.permissions?.map((p) => p?.name).includes("ADMIN") ||
+              user?.permissions?.map((p) => p?.name).includes("SCIENTIST") ||
+              user?.permissions?.map((p) => p?.name).includes("TEACHER")) && (
+              <div>
+                Need a new block?<br></br>
+                <a href="https://mindhive.notion.site/18bd80abf4c480749952e3c0498fab29?pvs=105" target="_blank">
+                <button>Fill a Request Form</button>
+                </a>
+              </div>
+            )}
+          </div>
         </div>
+        
       </div>
 
       <Links />
