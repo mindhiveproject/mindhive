355df6142c13129e9db7aeae9743d961d276a591
Franck Porteous <franck.porteous@protonmail.com>
2025-04-29 19:35:11 -0400

    propagating changes (d98937b) to DataTool flow

--- frontend/components/Builder/Project/DataJournal/Document/Sections/HypothesisVisualizer/Controller/Templates.js
+++ frontend/components/Builder/Project/DataJournal/Document/Sections/HypothesisVisualizer/Controller/Templates.js
@@ -31,11 +31,6 @@ for key, value in parameters.items():
         # Convert value to int when appending to conditionRanks
         conditionRanks.append(int(value))  # make integers
 
-print("conditionRanks") 
-print(conditionRanks) 
-print("conditionNames") 
-print(conditionNames) 
-
 # OPTIONS ####################################################################
 
 graphTitle = parameters["graphTitle"] if "graphTitle" in parameters else "Effect of Condition on Performance"
@@ -175,11 +170,7 @@ plt.savefig(img_buffer, format='png', bbox_inches='tight')
 img_buffer.seek(0)
 img_base64 = base64.b64encode(img_buffer.read()).decode('utf-8')
 fig_html = f'<img src="data:image/png;base64,{img_base64}" />'
-
-  
-# Display the plot
-js.render_html(html_output, fig_html)
-  `;
+`;
 
   const sectionCodeEnd = `  
 # Display the plot
