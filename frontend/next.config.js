125c61b6d972184fa86892d470d9818eb2008bbd
Yury-Shevchenko <shevchenko_yury@mail.ru>
2025-06-23 11:30:56 +0200

    use next translate plugin

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,13 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ['en-us', 'es-es', 'zh-cn', 'fr-fr', 'ar-ae', 'hi-in', 'hi-ma', 'ru-ru', 'nl-nl', 'pt-br'],
-    defaultLocale: 'en-us',
-    localeDetection: true
-  }
+    locales: [
+      "en-us",
+      "es-es",
+      "zh-cn",
+      "fr-fr",
+      "ar-ae",
+      "hi-in",
+      "hi-ma",
+      "ru-ru",
+      "nl-nl",
+      "pt-br",
+    ],
+    defaultLocale: "en-us",
+    localeDetection: true,
+  },
 };
 
-const nextTranslate = require("next-translate");
+const nextTranslate = require("next-translate-plugin");
 
 // module.exports = nextConfig
 module.exports = nextTranslate(nextConfig);
