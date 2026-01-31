8b1a52083d26db886e224ffec04093d3fd0f4955
Franck Porteous <franck.porteous@protonmail.com>
2026-01-31 12:17:24 -0500

    (public) fixed undefined creator of public studies for discover

--- frontend/components/Studies/Bank/StudyCard.js
+++ frontend/components/Studies/Bank/StudyCard.js
@@ -8,52 +8,32 @@ export default function StudyCard({ user, study, url, id, name, studiesInfo }) {
   const { t } = useTranslation("builder");
   const imageURL = study?.image?.image?.publicUrlTransformed;
 
+  const linkHref = user
+    ? { pathname: url, query: { [name]: study[id] } }
+    : { pathname: url };
+
   return (
     <StyledStudyCard>
       {studiesInfo && (
         <StudyOptions user={user} study={study} studiesInfo={studiesInfo} />
       )}
-      {user ? (
-        <Link
-          href={{
-            pathname: url,
-            query: { [name]: study[id] },
-          }}
-        >
-          <div className="studyImage">
-            {imageURL ? (
-              <img src={imageURL} alt={study?.title} />
-            ) : (
-              <div className="noImage"></div>
+      <Link href={linkHref}>
+        <div className="studyImage">
+          {imageURL ? (
+            <img src={imageURL} alt={study?.title} />
+          ) : (
+            <div className="noImage"></div>
+          )}
+        </div>
+        <div className="cardInfo">
+          <div className="studyHeader">
+            <h2>{study.title}</h2>
+            {study?.author?.username && (
+              <span>{t("createdBy", { username: study.author.username })}</span>
             )}
           </div>
-          <div className="cardInfo">
-            <div className="studyHeader">
-              <h2>{study.title}</h2>
-              <span>{t("createdBy", { username: study?.author?.username })}</span>
-            </div>
-          </div>
-        </Link>
-      ) : (
-        <Link
-          href={{
-            pathname: url,
-          }}
-        >
-          <div className="studyImage">
-            {imageURL ? (
-              <img src={imageURL} alt={study?.title} />
-            ) : (
-              <div className="noImage"></div>
-            )}
-          </div>
-          <div className="cardInfo">
-            <div className="studyHeader">
-              <h2>{study.title}</h2>
-            </div>
-          </div>
-        </Link>
-      )}
+        </div>
+      </Link>
     </StyledStudyCard>
   );
 }
