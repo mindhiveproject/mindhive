01fac7954eaff1faa0e9506c667e00d6b8b4598b
Franck Porteous <franck.porteous@protonmail.com>
2026-04-09 17:29:08 -0400

    (datatool) nav panel ui

--- frontend/components/Builder/Project/DataJournal/Helpers/CreateJournal.js
+++ frontend/components/Builder/Project/DataJournal/Helpers/CreateJournal.js
@@ -1,8 +1,14 @@
 import { useMutation } from "@apollo/client";
+import { useState } from "react";
+import useTranslation from "next-translate/useTranslation";
 
+import DropdownSelect from "../../../../DesignSystem/DropdownSelect";
 import { CREATE_VIZJOURNAL } from "../../../../Mutations/VizJournal";
 import { ADD_VIZPART } from "../../../../Mutations/VizPart";
 import { GET_DATA_JOURNALS } from "../../../../Queries/DataArea";
+
+const ADD_NEW_JOURNAL_VALUE = "__add_new_journal__";
+const ADD_TEMPLATE_JOURNAL_VALUE = "__add_template_journal__";
 
 export default function CreateJournal({
   projectId,
@@ -10,6 +16,7 @@ export default function CreateJournal({
   createNewJournalCollection,
   journalCollections,
 }) {
+  const { t } = useTranslation("builder");
   const [createJournal, { data, loading, error }] = useMutation(
     CREATE_VIZJOURNAL,
     {
@@ -132,9 +139,49 @@ export default function CreateJournal({
     }
   };
 
+  const addNewJournalFromTemplate = () => {
+    console.log("addNewJournalFromTemplate");
+  };
+
+  const [journalDropdownValue, setJournalDropdownValue] = useState(undefined);
+
+  const handleJournalDropdownChange = (next) => {
+    if (next === ADD_NEW_JOURNAL_VALUE) {
+      addNewJournal();
+      setJournalDropdownValue(undefined);
+      return;
+    }
+    if (next === ADD_TEMPLATE_JOURNAL_VALUE) {
+      addNewJournalFromTemplate();
+      setJournalDropdownValue(undefined);
+      return;
+    }
+    setJournalDropdownValue(next);
+  };
+
+  const journalDropdownOptions = [
+    {
+      value: ADD_NEW_JOURNAL_VALUE,
+      label: t("dataJournal.sideNav.addJournalScratch", "Start from scratch"),
+    },
+    {
+      value: ADD_TEMPLATE_JOURNAL_VALUE,
+      label: t("dataJournal.sideNav.addJournalTemplate", "Use a template"),
+    },
+  ];
+
   return (
-    <div className="menuButton" onClick={() => addNewJournal()}>
-      Create new journal
+    <div className="createJournalBtn">
+      <DropdownSelect
+        value={journalDropdownValue}
+        options={journalDropdownOptions}
+        onChange={handleJournalDropdownChange}
+        ariaLabel={t("dataJournal.sideNav.add", "Add")}
+        placeholder={t("dataJournal.sideNav.add", "Add")}
+        icon="+"
+        triggerStyle={{ backgroundColor: "#F6F9F8", border: "none", fontWeight: 600, fontSize: "16px", color: "#5D5763" }}
+      />
+
     </div>
   );
 }
