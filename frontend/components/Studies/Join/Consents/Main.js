2070f10c25ac2758723c00db445d7a55689773c7
Franck Porteous <franck.porteous@protonmail.com>
2026-09-02 09:55:56 -0400

    (signup) accessibility in auth and study join components

--- frontend/components/Studies/Join/Consents/Main.js
+++ frontend/components/Studies/Join/Consents/Main.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
 import Link from "next/link";
 
 import useForm from "../../../../lib/useForm";
@@ -8,10 +7,10 @@ import JoinStudy from "../JoinStudy";
 import ConsentForm from "./Form";
 import useTranslation from "next-translate/useTranslation";
 import Button from "../../../DesignSystem/Button";
+import { StyledConsent } from "../../../styles/StyledJoinStudyFlow";
 
 export default function Consents({ query, user, study }) {
-  const router = useRouter();
-  const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
 
   const { inputs, handleChange } = useForm({
     ...query,
@@ -19,7 +18,6 @@ export default function Consents({ query, user, study }) {
 
   const [consentId, setConsentId] = useState(query?.consent || null);
 
-  const { settings } = study;
   const consents = study?.consent || [];
   const [currentConsent] = consents?.filter((con) => con.id === consentId);
 
@@ -32,83 +30,65 @@ export default function Consents({ query, user, study }) {
   }
 
   useEffect(() => {
-    // Function to handle change of consent id via a query
-    const handleRefresh = () => {
-      setConsentId(query?.consent);
-    };
-    handleRefresh();
+    setConsentId(query?.consent);
   }, [query]);
 
+  const skipHref = {
+    pathname: `/join/consent-skipped`,
+    query: {
+      id: study?.id,
+      studyId: study?.id,
+    },
+  };
+
   return (
-    <div>
+    <StyledConsent>
       <ConsentForm
         consent={currentConsent}
-        settings={settings}
         userInfo={query}
         inputs={inputs}
         handleChange={handleChange}
       />
 
-      {!isItTheLastConsent && (
-        <Link
-          href={{
-            pathname: `/join/consent`,
-            query: {
+      <div className="actions">
+        {!isItTheLastConsent && (
+          <Link
+            href={{
+              pathname: `/join/consent`,
+              query: {
+                ...inputs,
+                id: study?.id,
+                consent: nextConsentId,
+                [`consent-${consentId}`]: `agree`,
+              },
+            }}
+          >
+            <Button variant="filled">
+              {t("consent.form.iAgreeNext", {}, { default: "I agree, next" })}
+            </Button>
+          </Link>
+        )}
+
+        {isItTheLastConsent && (
+          <JoinStudy
+            user={user}
+            study={study}
+            userInfo={{
               ...inputs,
-              id: study?.id,
-              consent: nextConsentId,
               [`consent-${consentId}`]: `agree`,
-            },
-          }}
-        >
-          <Button variant="filled" style={{ width: "100%" }}>
-            {t("consent.form.iAgreeNext", {}, { default: "I agree, next" })}
-          </Button>
-        </Link>
-      )}
+            }}
+            btnName={t("consent.form.iAgreeJoin", {}, {
+              default: "I agree, join the study",
+            })}
+          />
+        )}
 
-      {!isItTheLastConsent && (
-        <Link
-          href={{
-            pathname: `/join/consent-skipped`,
-            query: {
-              studyId: study?.id,
-            },
-          }}
-        >
-          <Button variant="outline" style={{ width: "100%" }}>
+        <Link href={skipHref}>
+          <Button variant="outline">
             {t("consent.form.skip", {}, { default: "Skip consent" })}
           </Button>
         </Link>
-      )}
-
-      {isItTheLastConsent && (
-        <JoinStudy
-          user={user}
-          study={study}
-          userInfo={{
-            ...inputs,
-            [`consent-${consentId}`]: `agree`,
-          }}
-          btnName={t("consent.form.iAgreeJoin", {}, { default: "I agree, join the study" })}
-          variant="filled"
-        />
-      )}
-
-      {isItTheLastConsent && (
-        <Link
-          href={{
-            pathname: `/join/consent-skipped`,
-            query: {
-              studyId: study?.id,
-            },
-          }}
-        >
-          <Button variant="outline" style={{ width: "100%" }}>
-            {t("consent.form.skip", {}, { default: "Skip consent" })}
-          </Button>
-        </Link>
-      )}
-    </div>
+      </div>
+    </StyledConsent>
   );
 }
